4db751df357f078411c81ce8b2e926a28c2454b5
Nutree Admin <info+bitbucket@nutree.gr>
2020-02-09 22:37:51 +0100

    first implementation of /en/ website

--- el/js/common.js
+++ el/js/common.js
@@ -1,19 +1,24 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  // Show/Hide mobile menu
+    // Show/Hide mobile menu
 
-  $('.top-nav-icon').click(function(e) {
-    e.preventDefault();
-    $('.top-nav-menu').toggleClass('active');
-  });
+    $('.top-nav-icon.icon-hamburger').click(function (e) {
+        e.preventDefault();
+        $('.top-nav-menu').toggleClass('active');
+    });
+
+    $('.top-nav-icon.icon-language').click(function (e) {
+        e.preventDefault();
+        $('.top-nav-menu').toggleClass('active');
+    });
 });
 
-window.setTimeout(function() {
-  $(".top-nav").addClass("unmask");
-}, 500
+window.setTimeout(function () {
+        $(".top-nav").addClass("unmask");
+    }, 500
 );
 
-window.setTimeout(function() {
-  $(".header").addClass("unmask");
-}, 1000
+window.setTimeout(function () {
+        $(".header").addClass("unmask");
+    }, 1000
 );
