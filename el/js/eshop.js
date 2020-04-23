62982495026e4eac5ec915f609f37beb35309851
Nutree Admin <info+bitbucket@nutree.gr>
2020-04-23 20:45:09 +0200

    eshop: implement integration with shopify (only boxes and checkout redirect so far)

--- el/js/eshop.js
+++ el/js/eshop.js
@@ -42,30 +42,27 @@
                     "Apple Cinnamon",
                 "defaultVariantId":
                     "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zMTY0OTMwNDgzODIwNQ=="
-            },
+            }
+        },
+
+        boxes: {
             smallBox: {
-                "id":
-                    "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzQ0MzQ4NjAyMTIyODU=",
-                "title":
-                    "Small Box",
-                "defaultVariantId":
-                    "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zMTY2NjczNDU2MzM4OQ=="
+                id: "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzQ0MzQ4NjAyMTIyODU=",
+                title: "Small Box",
+                defaultVariantId: "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zMTY2NjczNDU2MzM4OQ==",
+                totalBars: 12
             },
             mediumBox: {
-                "id":
-                    "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzQ0MzQ4NjAzNDMzNTc=",
-                "title":
-                    "Medium Box",
-                "defaultVariantId":
-                    "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zMTY2NjczNTY3NzUwMQ=="
+                id: "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzQ0MzQ4NjAzNDMzNTc=",
+                title: "Medium Box",
+                defaultVariantId: "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zMTY2NjczNTY3NzUwMQ==",
+                totalBars: 24
             },
             largeBox: {
-                "id":
-                    "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzQ0MzQ4NjA1MDcxOTc=",
-                "title":
-                    "Large Box",
-                "defaultVariantId":
-                    "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zMTY2NjczNjg1NzE0OQ=="
+                id: "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0LzQ0MzQ4NjA1MDcxOTc=",
+                title: "Large Box",
+                defaultVariantId: "Z2lkOi8vc2hvcGlmeS9Qcm9kdWN0VmFyaWFudC8zMTY2NjczNjg1NzE0OQ==",
+                totalBars: 48
             }
         },
 
@@ -78,6 +75,23 @@
             return JSON.parse(localStorage.getItem(STORAGE_KEY) || '{}');
         },
 
+        getBarLimit: () => {
+            return NutreeJS.boxes[NutreeJS.getStorage().selectedBox].totalBars;
+        },
+
+        getRemainingBars: () => {
+            let barCount = 0;
+            try {
+                let barNames = Object.keys(NutreeJS.products);
+                barCount = barNames.reduce((total, bar) => {
+                    return total + (NutreeJS.getStorage().selectedBars[bar] || 0);
+                }, 0);
+            } catch (ignore) {
+            }
+
+            return NutreeJS.getBarLimit() - barCount;
+        },
+
         store: (o) => {
             const stored = JSON.parse(localStorage.getItem(STORAGE_KEY) || '{}');
             jQuery.extend(true, stored, o);
@@ -85,16 +99,45 @@
             return stored;
         },
 
-        init: () => {
+        initCheckout: () => {
+            let storage = NutreeJS.getStorage();
+            if (!storage.checkoutId) {
+                console.debug("creating new shopify checkout cart");
+                NutreeJS.checkout = NutreeJS.client.checkout.create();
+                NutreeJS.checkout.then(c => {
+                    storage.checkoutId = c.id;
+                    NutreeJS.store(storage);
+                });
+            } else {
+                console.debug("fetching existing shopify checkout cart");
+                NutreeJS.checkout = NutreeJS.client.checkout.fetch(storage.checkoutId);
+            }
+            return NutreeJS.checkout;
+        },
+
+        init: (config) => {
             addEventListener('storage', function (e) {
                 if (e.key !== STORAGE_KEY || e.newValue === e.oldValue) {
                     return;
                 }
                 console.debug(e.newValue);
                 localStorage.setItem(STORAGE_KEY, e.newValue);
+                (config.storageSyncCallback || function () {
+                })();
+            });
+
+            NutreeJS.initCheckout().then(c => {
+                console.debug(c);
+                if (!!c.completedAt) {
+                    console.debug("resetting shopify checkout cart");
+                    let storage = NutreeJS.getStorage();
+                    storage.checkoutId = null;
+                    NutreeJS.initCheckout();
+                }
+            }).catch(error => {
+                console.error(error);
+                NutreeJS.initCheckout();
             })
         }
     };
-
-    NutreeJS.init();
 })();
